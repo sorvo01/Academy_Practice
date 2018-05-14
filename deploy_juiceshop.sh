84cc151b6ce6617cf47ad5f9193e6595f62e91c8
sorvo01 <39085854+sorvo01@users.noreply.github.com>
2018-05-14 17:07:32 +0300

    Update 1
    Renewed Docker deployment
    Added comments

--- deploy_juiceshop.sh
+++ deploy_juiceshop.sh
@@ -3,8 +3,7 @@
 tput setaf 6
 echo -e ""
 echo -e "------------------------------------"
-echo -e "Begin of "
-echo -e "Please connect to http://<ip-address>:3000"
+echo -e "Beginning of OWASP Juice Shop installation..."
 echo -e "------------------------------------"
 echo -e ""
 
@@ -14,25 +13,22 @@ echo -e "Installing Docker..."
 echo -e "------------------------------------"
 
 {
-apt-get update
-apt-get install -y apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-
-apt-get update
-
-apt-get purge lxc-docker
-
-apt-cache policy docker-engine
-
-apt-get update
-
-apt-get upgrade -y
-
-apt-get install -y linux-image-extra-$(uname -r)
-
-apt-get install -y docker-engine
+#Uninstall older versions
+sudo apt-get remove docker docker-engine docker.io
+#Update the apt package index
+sudo apt-get update
+#Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+#Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
+#Add Docker repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#Update the apt package index.
+sudo apt-get update
+#Install the latest version of Docker CE, or go to the next step to install a specific version
+sudo apt-get install docker-ce
+#Verify Docker installation
+sudo docker run hello-world
 } &> /dev/null
 
 echo -e "Verifying Docker installation..."
@@ -63,14 +59,16 @@ fi
 
 tput setaf 2
 echo -e "------------------------------------"
-echo -e "Downloading OWASP Juice Shopp..."
+echo -e "Downloading OWASP Juice Shop..."
 echo -e "------------------------------------"
 
+#Download OWASP Juice Shop Docker image
 docker pull bkimminich/juice-shop &> /dev/null
 
 echo -e "Launching OWASP Juice Shopp..."
 echo -e "------------------------------------"
 
+#Launch OWASP Juice Shop
 docker run -d -p 3000:3000 bkimminich/juice-shop &> /dev/null
 tput setaf 6
 echo -e ""
