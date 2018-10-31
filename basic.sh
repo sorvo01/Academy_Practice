a9c2cd89a816740aa2738d22fbbc13c6da3add40
sorvo01 <39085854+sorvo01@users.noreply.github.com>
2018-10-31 15:01:43 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,4 +1,5 @@
+arp-scan -I eth0 --srcaddr=DE:AD:BE:EF:CA:BA <ip>/24
 nmap -sC sV <ip> -oA nmap/<ip>
 apt-get install cewl -y
+dirb http://
 cewl http://<site> > list.txt
-arp-scan -I eth0 --srcaddr=DE:AD:BE:EF:CA:BA <ip>/24
